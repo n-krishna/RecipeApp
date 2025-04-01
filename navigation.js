1ac6b97d26ead8818719063630fc81f7bdfc31fa
Nithin Krishna <nkrishna6238@gmail.com>
2025-03-31 21:18:44 -0400

    modified the code

--- navigation.js
+++ navigation.js
@@ -1,10 +1,8 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
-import AddRecipeScreen from "./screens/AddRecipeScreen";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
-import RecipeDetailScreen from "./screens/RecipeDetailScreen";
 import SignupScreen from "./screens/SignupScreen";
 
 const Stack = createStackNavigator();
@@ -16,8 +14,6 @@ export default function AppNavigator() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="RecipeDetail" component={RecipeDetailScreen} />
-        <Stack.Screen name="AddRecipe" component={AddRecipeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
